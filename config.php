3051df7ad59b89f0600b2e79b3ac3b21d59b80f3
Zannie766 <thaituvan44@gmail.com>
2025-05-31 07:26:03 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'base' => '/PTITCNTT01/JFmvc/',
+    'baseURL' => 'http://localhost/PTITCNTT01/JFmvc/',
+    'assets' => '/PTITCNTT01/JFmvc/assets/',
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'jellyfish_db',
+        'username' => 'root',
+        'password' => ''
+    )
+);
